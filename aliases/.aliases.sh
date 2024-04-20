e50994b47fcb317dc7350c0fd693a167fcea1032
Bernardo Botella Corbi <contacto@bernardobotella.com>
2024-04-20 08:43:33 -0700

    Adds eza aliases

--- aliases/.aliases.sh
+++ aliases/.aliases.sh
@@ -1,2 +1,4 @@
 alias cat="bat"
 alias ggb="./gradlew clean && ./gradlew build"
+alias ls="eza --color=always --long --git --icons=always --no-time --no-user --no-permissions"
+alias lst="eza --tree --level=2"
